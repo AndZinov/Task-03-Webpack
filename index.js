3fcedc54e0ef8272beb91ec16ec9a9a7a5c2750f
Andrey Zinovev <andreyzinovv@gmail.com>
2022-12-29 17:38:07 +0300

    task-03,04

---
+++ index.js
@@ -0,0 +1,10 @@
+import "./index.css";
+
+import JS_IMAGE from './asset/js.jpg'
+
+const jsImageHTML = document.createElement('img');
+jsImageHTML.src = JS_IMAGE;
+
+document.body.append(jsImageHTML);
+
+console.log('Hello World!');
